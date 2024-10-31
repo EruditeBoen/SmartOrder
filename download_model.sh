9e3a599cfa65dcdde3bc45351d18535210198b3c
EruditeBoen <boen.erudition@gmail.com>
2024-10-31 16:02:27 -0400

    Update download_model.sh

--- download_model.sh
+++ download_model.sh
@@ -3,5 +3,9 @@
 curl "https://s3.ap-northeast-2.wasabisys.com/pinto-model-zoo/307_YOLOv7/no-postprocess/resources.tar.gz" -o resources.tar.gz
 tar -zxvf resources.tar.gz
 rm resources.tar.gz
+cd models
+cp ../resources/yolov7_384x640.onnx .
+cd ../
+rm -rf resources
 
 echo Download finished.
